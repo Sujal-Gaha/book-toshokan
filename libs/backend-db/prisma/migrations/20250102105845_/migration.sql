dda980e7c4bd146cb1303a1610770923081661d2
Sujal-Gaha <suzalgahamagar@gmail.com>
2025-01-02 16:44:59 +0545

    removed unwanted codes

--- libs/backend-db/prisma/migrations/20250102105845_/migration.sql
+++ libs/backend-db/prisma/migrations/20250102105845_/migration.sql
@@ -1,14 +1,20 @@
-/*
-  Warnings:
+-- CreateEnum
+CREATE TYPE "UserRoleEnum" AS ENUM ('ADMIN', 'USER');
 
-  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-
-*/
 -- CreateEnum
 CREATE TYPE "ReadStatusEnum" AS ENUM ('TO_READ', 'READ', 'CURRENTLY_READING');
 
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "role" SET DEFAULT 'USER';
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "image" TEXT,
+    "role" "UserRoleEnum" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "Author" (
@@ -33,9 +39,12 @@ CREATE TABLE "Book" (
     "id" TEXT NOT NULL,
     "authorId" TEXT NOT NULL,
     "categoryId" TEXT NOT NULL,
-    "readStatus" "ReadStatusEnum" NOT NULL,
+    "readStatus" "ReadStatusEnum",
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "subImages" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "pages" INTEGER NOT NULL,
     "publishedOn" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Book_pkey" PRIMARY KEY ("id")
@@ -46,7 +55,8 @@ CREATE TABLE "Feedback" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "bookId" TEXT NOT NULL,
-    "rating" INTEGER NOT NULL,
+    "averageRating" DOUBLE PRECISION NOT NULL,
+    "totalRatings" INTEGER NOT NULL,
     "comment" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
@@ -79,13 +89,19 @@ CREATE TABLE "UserBookSubscription" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Book_name_key" ON "Book"("name");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "Feedback_bookId_key" ON "Feedback"("bookId");
 
 -- AddForeignKey
 ALTER TABLE "Book" ADD CONSTRAINT "Book_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Author"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
